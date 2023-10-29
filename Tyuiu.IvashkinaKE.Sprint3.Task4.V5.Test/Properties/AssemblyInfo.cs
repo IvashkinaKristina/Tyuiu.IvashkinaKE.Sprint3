9c88ea3d172969684019b55028b76634ebdcfa5d
Tyuiu.IvashkinaKE@outlook.com <Tyuiu.IvashkinaKE@outlook.com>
2023-10-29 18:19:11 +0500

    Task4

---
+++ Tyuiu.IvashkinaKE.Sprint3.Task4.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvashkinaKE.Sprint3.Task4.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvashkinaKE.Sprint3.Task4.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("01ebf725-63fd-4d7a-a2cf-173a3f84d90e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
